405a25af0e84042e5a1aaba38e2040b90a6fddb1
Bahruz Shabili <bahruz.dsh@code.edu.az>
2024-07-13 14:16:30 +0400

    Created new project

---
+++ src/test/java/az/edu/turing/tinderapplication/TinderApplicationTests.java
@@ -0,0 +1,13 @@
+package az.edu.turing.tinderapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
